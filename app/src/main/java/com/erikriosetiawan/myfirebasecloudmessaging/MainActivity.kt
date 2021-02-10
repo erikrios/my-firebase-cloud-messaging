5a28296d733d39c33556fecae07a22d4c8546e6f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-10 19:34:55 +0700

    Implement the subscribe token

--- app/src/main/java/com/erikriosetiawan/myfirebasecloudmessaging/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/myfirebasecloudmessaging/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.erikriosetiawan.myfirebasecloudmessaging
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.myfirebasecloudmessaging.databinding.ActivityMainBinding
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +19,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSubscribe.setOnClickListener {
+            FirebaseMessaging.getInstance().subscribeToTopic("news")
+            val message = getString(R.string.msg_subscribed)
+            Log.d(TAG, message)
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnToken.setOnClickListener {
+            val deviceToken = MyFirebaseMessagingService
+            val message = getString(R.string.msg_token_fmt, deviceToken)
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "Refreshed token: $deviceToken")
+            FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+                val msg = getString(R.string.msg_token_fmt, token)
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "Refreshed token: $token")
+            }
+        }
     }
 }
