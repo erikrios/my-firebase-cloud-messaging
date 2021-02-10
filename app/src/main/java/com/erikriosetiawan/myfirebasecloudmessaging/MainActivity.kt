155c4bf6fcb25c6e4ecdd51106bc68a581f7273a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-10 19:28:16 +0700

    Integrated the view binding

--- app/src/main/java/com/erikriosetiawan/myfirebasecloudmessaging/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/myfirebasecloudmessaging/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.erikriosetiawan.myfirebasecloudmessaging
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.myfirebasecloudmessaging.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
